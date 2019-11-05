c217ef404b087ef31fadfe9f1e05ce51768e0728
Jose Antonio Parejo Maestre <japarejo@gmail.com>
2019-11-05 09:42:12 +0100

    Actualización de anotaciones para últimas versiones de Spring

--- src/test/java/com/japarejo/Springdatajpaexercise/SpringDataJpaExerciseApplicationTests.java
+++ src/test/java/com/japarejo/Springdatajpaexercise/SpringDataJpaExerciseApplicationTests.java
@@ -8,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@ContextConfiguration
+@SpringBootTest(classes= {SpringDataJpaExerciseApplicationTests.class})
 public class SpringDataJpaExerciseApplicationTests {
 
 	@Test
